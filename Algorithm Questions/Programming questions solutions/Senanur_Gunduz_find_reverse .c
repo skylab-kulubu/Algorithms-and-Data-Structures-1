2441af47bc8e596413c15028daeda55406ed498a
Senanur Gündüz <senanurgunduz7@gmail.com>
2022-11-06 21:49:27 +0300

    Create Senanur_Gunduz_find_reverse .c

---
+++ Algorithm Questions/Programming questions solutions/Senanur_Gunduz_find_reverse .c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void f(int a){
+	int t=0;
+	while(a != 0)
+    { 
+        t = (t * 10) + (a % 10);
+        a /= 10;
+    }
+
+    printf("%d \n", t);
+	 
+        return 0;	
+}
+
+int main(int argc, char *argv[]) {
+    //bu kısım sonuca bakmadan bir şey üretmeye çalıştığım kısım, fonkiyon ise sonuca baktıktan sonra çözümü anlayıp yazdığım kısım.
+	int i,t;
+	int a,basamak;
+	int sayac=0;
+	
+	printf("Lutfen sayiyi giriniz: ");
+	scanf("%d",&a);
+	
+	f(a);
+	
+	basamak= log10(a);
+	
+	for(i=0;i<=basamak;i++){
+		t=a/pow(10,(basamak-i));
+		a-= (t*pow(10,(basamak-i)));
+		t*=pow(10,i);
+		sayac+= t;
+		
+		}
+	
+	printf("%d",sayac);
+	
+	return 0;
+}
